2fb3c602b6177792dac285795b58252cf9c5a195
jusoyeong <syju05@gmail.com>
2026-03-17 13:09:55 +0900

    Add files via upload

---
+++ homework01.php
@@ -0,0 +1,22 @@
+<?php
+$n = 30; // 원하는 숫자 n을 설정하세요.
+$sum = 0;
+$product = 1;
+
+echo "1부터 $n 까지의 숫자: <br>";
+
+for ($i = 1; $i <= $n; $i++) {
+    
+    echo $i . " ";
+    
+    
+    $sum += $i;
+    
+    
+    $product *= $i;
+}
+
+echo "<br><br>";
+echo "<strong>전체 합:</strong> " . $sum . "<br>";
+echo "<strong>전체 곱:</strong> " . $product;
+?>
